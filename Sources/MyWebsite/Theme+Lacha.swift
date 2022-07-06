a38d882beabcb4e57ebb1e2e5f71e49cf6201e86
Achraf Trabelsi <trabelsiachraf.mobile@gmail.com>
2022-07-06 14:30:56 +0100

    change files directory

--- Sources/MyWebsite/Theme+Lacha.swift
+++ Sources/MyWebsite/Theme+Lacha.swift
@@ -14,7 +14,7 @@ extension Theme where Site == MyWebsite {
     static var lacha: Self {
         Theme(
             htmlFactory: LachaHTMLFactory(),
-            resourcePaths: ["Resources/LachaTheme/styles.css"]
+            resourcePaths: ["/styles.css"]
         )
     }
 }
